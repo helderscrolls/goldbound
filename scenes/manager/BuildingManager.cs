19efc52280211022101c6efe1574e997f2eb626f
helderscrolls <nortehelder@gmail.com>
2025-02-17 20:50:17 +0100

    refactor(buildings): added an IsDeletable to BuildingResource so we can flag if buildings can be deleted or not

--- scenes/manager/BuildingManager.cs
+++ scenes/manager/BuildingManager.cs
@@ -131,7 +131,10 @@ public partial class BuildingManager : Node
 	{
 		var rootCell = hoveredGridArea.Position;
 		var buildingComponent = GetTree().GetNodesInGroup(nameof(BuildingComponent)).Cast<BuildingComponent>()
-			.FirstOrDefault((buildingComponent) => buildingComponent.GetGridCellPosition() == rootCell);
+			.FirstOrDefault((buildingComponent) =>
+			{
+				return buildingComponent.BuildingResource.IsDeletable && buildingComponent.GetGridCellPosition() == rootCell;
+			});
 
 		if (buildingComponent == null) return;
 
