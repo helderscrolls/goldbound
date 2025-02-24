9714ee3bee1938bd40c932f925fd33fb503b03e2
helderscrolls <nortehelder@gmail.com>
2025-02-24 16:47:45 +0100

    feat(UX): add placement indicators for building placement

--- scenes/building/BuildingGhost.cs
+++ scenes/building/BuildingGhost.cs
@@ -4,6 +4,19 @@ namespace Game.Building;
 
 public partial class BuildingGhost : Node2D
 {
+	private Node2D topLeft;
+	private Node2D bottomLeft;
+	private Node2D topRight;
+	private Node2D bottomRight;
+
+	public override void _Ready()
+	{
+		topLeft = GetNode<Node2D>("TopLeft");
+		bottomLeft = GetNode<Node2D>("BottomLeft");
+		topRight = GetNode<Node2D>("TopRight");
+		bottomRight = GetNode<Node2D>("BottomRight");
+	}
+
 	public void SetInvalid()
 	{
 		Modulate = Colors.Red;
@@ -12,5 +25,12 @@ public partial class BuildingGhost : Node2D
 	public void SetValid()
 	{
 		Modulate = Colors.White;
+	}
+
+	public void SetDimensions(Vector2I dimensions)
+	{
+		bottomLeft.Position = dimensions * new Vector2I(0, 64);
+		bottomRight.Position = dimensions * new Vector2I(64, 64);
+		topRight.Position = dimensions * new Vector2I(64, 0);
 	}
 }
