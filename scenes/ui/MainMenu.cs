722445aea9eb0a72664889f54cc91c878e559209
helderscrolls <nortehelder@gmail.com>
2025-02-24 15:54:59 +0100

    feat(UI): add LevelSelectScreen to MainMenu

--- scenes/ui/MainMenu.cs
+++ scenes/ui/MainMenu.cs
@@ -6,17 +6,25 @@ namespace Game.UI;
 public partial class MainMenu : Node
 {
 	private Button playButton;
+	private Control mainMenuContainer;
+	private LevelSelectScreen levelSelectScreen;
 
 	public override void _Ready()
 	{
 		playButton = GetNode<Button>("%PlayButton");
+		mainMenuContainer = GetNode<Control>("%MainMenuContainer");
+		levelSelectScreen = GetNode<LevelSelectScreen>("%LevelSelectScreen");
+
+		levelSelectScreen.Visible = false;
+		mainMenuContainer.Visible = true;
 
 		playButton.Connect(Button.SignalName.Pressed, Callable.From(OnPlayButtonPressed));
 	}
 
 	private void OnPlayButtonPressed()
 	{
-		LevelManager.Instance.ChangeToLevel(0);
+		mainMenuContainer.Visible = false;
+		levelSelectScreen.Visible = true;
 	}
 
 }
