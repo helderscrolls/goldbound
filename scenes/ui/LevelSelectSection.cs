b692aa21f5e64ea0e9da9be42db0198a199deeda
helderscrolls <nortehelder@gmail.com>
2025-02-24 15:31:58 +0100

    feat(LevelSelectScreen): added logic to display all the current levels and being able to access them from the level select screen

---
+++ scenes/ui/LevelSelectSection.cs
@@ -0,0 +1,43 @@
+
+using Game.Resources.Level;
+using Godot;
+
+namespace Game.UI;
+
+public partial class LevelSelectSection : PanelContainer
+{
+	[Signal]
+	public delegate void LevelSelectedEventHandler(int levelIndex);
+
+	private Button button;
+	private Label resourceCountLabel;
+	private Label levelNumberLabel;
+	private int levelIndex;
+
+	public override void _Ready()
+	{
+		button = GetNode<Button>("%Button");
+		resourceCountLabel = GetNode<Label>("%ResourceCountLabel");
+		levelNumberLabel = GetNode<Label>("%LevelNumberLabel");
+
+		button.Pressed += OnButtonPressed;
+	}
+
+	public void SetLevelDefinition(LevelDefinitionResource levelDefinition)
+	{
+		resourceCountLabel.Text = levelDefinition.StartingResourceCount.ToString();
+		levelNumberLabel.Text = levelDefinition.Id;
+
+	}
+
+	public void SetLevelIndex(int index)
+	{
+		levelIndex = index;
+		levelNumberLabel.Text = $"Level {index + 1}";
+	}
+
+	private void OnButtonPressed()
+	{
+		EmitSignal(SignalName.LevelSelected, levelIndex);
+	}
+}
